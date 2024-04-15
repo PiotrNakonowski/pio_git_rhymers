52f287c49b543cdb42ccb6a74809598da0c11b69
Piotr Nakonowski <240760@edu.p.lodz.pl>
2024-04-15 21:29:25 +0200

    2. Poprawa błędów z formatowaniem kodu w pliku HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,14 +4,15 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
 
     int totalRejected = 0;
 
-    public int reportRejected()
-    {
+    public int reportRejected() {
         return totalRejected;
     }
 
     public void countIn(int in)
     {
-        if (!callCheck() && in > peekaboo()) totalRejected++;
-        else super.countIn(in);
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
     }
 }
