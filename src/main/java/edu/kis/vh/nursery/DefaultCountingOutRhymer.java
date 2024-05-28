bb774f22641c703b0ab38ca3f4ab43d2ea07b9e2
Piotr Nakonowski <240760@edu.p.lodz.pl>
2024-05-28 21:43:11 +0200

    12. Usunięcie nieużywanego gettera.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -9,10 +9,6 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY;
 
-    public int getTotal() {
-        return total;
-    }
-
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
